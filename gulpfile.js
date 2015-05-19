663264c7636a3cc15892bd4f76f1022ec18021f4
nanqiu <travelofnature@gmail.com>
2015-05-20 06:39:15 +0800

    compress photo

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp');
 var watermark = require('gulp-watermark');
 var tap = require('gulp-tap');
+//var imagemin = require('gulp-imagemin');
+var imageminJpegtran = require('imagemin-jpegtran');
+var gm = require('gulp-gm');
 
 gulp.task('watermark', function() {
-    return gulp.src('./article/travel/newzealand/*.jpg')
-        .pipe(watermark({
-            image: './tool/watermark.png',
-						resize: '100%',
-            gravity: 'SouthEast'
+    return gulp.src('./src/travel/newzealand/*.jpg')
+        //        .pipe(watermark({
+        //            image: './src/watermark/newzealand.png',
+        //            resize: '100%',
+        //            gravity: 'SouthEast'
+        //        }))
+        //.pipe(smushit())
+        .pipe(gm(function(gmfile, done) {
+            gmfile.size(function(err, size) {
+                done(null, gmfile.resize(
+                    size.width * 0.5,
+                    size.height * 0.5
+                ));
+            });
         }))
+        .pipe(imageminJpegtran()())
         .pipe(gulp.dest('./dist'));
 });
